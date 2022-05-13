9479d79aee93178ce9074fd1e7a1a7bb9213bb79
Yves de Sá <yvesdsb98@gmail.com>
2022-05-13 19:02:31 -0300

    feat: create product items section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/shared/header/header.component';
-import { FooterComponent } from './components/shared/footer/footer.component';
 import { NavComponent } from './components/shared/nav/nav.component';
-import { FilterComponent } from './components/store/filter/filter.component';
 import { ProductListComponent } from './components/store/product-list/product-list.component';
 import { StoreComponent } from './components/store/store.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -19,9 +17,7 @@ import { ProductItemComponent } from './components/store/product-list/product-it
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
     NavComponent,
-    FilterComponent,
     ProductListComponent,
     ProductItemComponent,
     StoreComponent,
